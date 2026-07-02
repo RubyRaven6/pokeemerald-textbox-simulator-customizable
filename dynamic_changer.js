b008bc921ed21c58486c6f62a43232c4d954b37c
RubyRaven6 <swampertspawner@gmail.com>
2026-07-02 08:11:43 +0800

    first draft of changing text area width with dropdown

--- dynamic_changer.js
+++ dynamic_changer.js
@@ -6,17 +6,22 @@ var widthDropdown = document.getElementById("widthOptions");
 
 var lastSelected = widthDropdown.options[widthDropdown.selectedIndex].value
 
+var textareaWidth = document.querySelector(":root");
+
 console.log(lastSelected);
 
 widthDropdown.onchange = function(){
 
-    var textBox = document.getElementById("agencyDescription");    
-    currentText = textBox.value;
+    currentWidth = textBox.value;
+
+    dropdownTextbox_get();
 
     var selected = this.options[this.selectedIndex].value;
 
+    dropdownTextbox_set();
+
     //load the current text into the last selected value
-    sessionStorage.setItem(lastSelected, currentText);
+    sessionStorage.setItem(lastSelected, currentWidth);
 
 
     //load the new text in to the text box
@@ -24,3 +29,16 @@ widthDropdown.onchange = function(){
 
     lastSelected = selected;
 };
+
+// Function for getting a variable value
+function dropdownTextbox_get() {
+  // Get the styles (properties and values) for the root
+  var textareaWidthStyle = getComputedStyle(textareaWidth);
+  // Alert the value of the --textbox-width variable
+  alert("The value of --textbox-width is: " + textareaWidthStyle.getPropertyValue('--textbox-width'));
+}
+
+function dropdownTextbox_set() {
+  // Set the value of variable --textbox-width to another value
+  r.style.setProperty('--textbox-width', selected);
+}
